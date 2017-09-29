c93d0bb3ff63e245ac1cd2d7dc574e44a296a654
Scott Doxey <neogeek0101@gmail.com>
2017-09-28 22:53:49 -0400

    Store reference to BoxCollider2D.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof (BoxCollider2D))]
 public class PlayerController : MonoBehaviour {
 
     public LayerMask leftLayerMask;
@@ -43,6 +44,8 @@ public class PlayerController : MonoBehaviour {
     private readonly float jumpSpeed = 14.0f;
     private readonly int maxAvalibleJumps = 2;
 
+    private BoxCollider2D boxCollider;
+
     private Vector2 velocity = Vector2.zero;
     private int horizontalDirection = 1;
 
@@ -66,6 +69,12 @@ public class PlayerController : MonoBehaviour {
             }
 
         }
+    }
+
+    void Awake() {
+
+        boxCollider = gameObject.GetComponent<BoxCollider2D>();
+
     }
 
     void Start() {
@@ -377,7 +386,7 @@ public class PlayerController : MonoBehaviour {
 
     private void UpdateHitVectors() {
 
-        Bounds colliderBounds = gameObject.GetComponent<BoxCollider2D>().bounds;
+        Bounds colliderBounds = boxCollider.bounds;
 
         Vector2 rayCastSize = colliderBounds.size * 0.95f;
 
