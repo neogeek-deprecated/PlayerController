0cdbcd3ac485a54cb5432e4138dcf6cb02a448b8
Scott Doxey <neogeek0101@gmail.com>
2017-09-30 07:01:23 -0400

    Added reset script for testing.

---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Reset : MonoBehaviour {
+
+    private Vector3 originalPosition;
+
+    void Awake() {
+
+        originalPosition = gameObject.transform.position;
+
+    }
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+
+            ResetGameObject();
+
+        }
+
+    }
+
+    void ResetGameObject() {
+
+        gameObject.transform.position = originalPosition;
+
+    }
+
+}
