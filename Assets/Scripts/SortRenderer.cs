7e6b3bd19d524d41a1d5d4ba78562ca2e9675a12
Scott Doxey <neogeek0101@gmail.com>
2017-10-05 18:35:22 -0400

    Added SortRenderer utility.

---
+++ Assets/Scripts/SortRenderer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SortRenderer : MonoBehaviour {
+
+    public string sortingLayerName;
+    public new Renderer renderer;
+
+    void Awake() {
+
+        renderer.sortingLayerName = sortingLayerName;
+
+    }
+
+}
