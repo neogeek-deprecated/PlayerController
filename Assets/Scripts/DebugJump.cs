0bc220e8c71eda9c0ffc9964e6e871ede3473a81
Scott Doxey <neogeek0101@gmail.com>
2017-10-05 20:44:13 -0400

    Added debug code.

---
+++ Assets/Scripts/DebugJump.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugJump : MonoBehaviour {
+
+    private readonly float debugLineWidth = 2.0f;
+
+    [SerializeField]
+    private List<Vector3> fallingPositions = new List<Vector3>();
+
+    private Vector3 previousPosition = Vector3.zero;
+
+    private bool falling = false;
+
+    void Update() {
+
+        foreach (Vector3 position in fallingPositions) {
+
+            Debug.DrawLine(position + Vector3.left * debugLineWidth / 2, position + Vector3.right * debugLineWidth / 2, Color.red);
+
+        }
+
+        Vector3 currentPosition = gameObject.transform.position;
+
+        if (previousPosition.y > currentPosition.y && !falling) {
+
+            falling = true;
+
+            fallingPositions.Add(currentPosition);
+
+        } else if (previousPosition.y < currentPosition.y) {
+
+            falling = false;
+
+        }
+
+        previousPosition = currentPosition;
+
+    }
+
+}
